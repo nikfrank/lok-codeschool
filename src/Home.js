f6846bf7214ac62504960184219d99134a552c62
nik frank <nikbfrank@gmail.com>
2018-09-11 18:27:04 +0300

    home bg and float start

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import './Home.css';
 
+import background from './computer-lab-old.png';
+
 class Home extends Component {
   render(){
     return (
-      <div>Home goes here</div>
+      <div style={{
+        backgroundImage: `url(${background})`,
+        flexGrow: 1,
+        position: 'relative',
+        backgroundSize: 'cover',
+      }}>
+
+        <div style={{
+          position: 'absolute',
+          top: '30%',
+          left: '10%',
+          padding: 30,
+          borderRadius: 8,
+          fontSize: '2em',
+          backgroundColor: 'rgba(255, 150, 102, 0.875)',
+        }}>
+          Cutting Edge Web Code Curriculum
+        </div>
+      </div>
     );
   }
 }
