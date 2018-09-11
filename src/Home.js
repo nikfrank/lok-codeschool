52537d828ebcd8cd3b5e816c5251545a6933b793
nik frank <nikbfrank@gmail.com>
2018-09-11 23:09:40 +0300

    more on the home page, font... needs writeup

--- src/Home.js
+++ src/Home.js
@@ -2,27 +2,38 @@ import React, { Component } from 'react';
 import './Home.css';
 
 import background from './computer-lab-old.png';
+import solidity from './Solidity.png';
+import nodejs from './nodejs.png';
+import reactjs from './reactjs.png';
+
+import { NavLink } from 'react-router-dom';
 
 class Home extends Component {
   render(){
     return (
-      <div style={{
-        backgroundImage: `url(${background})`,
-        flexGrow: 1,
-        position: 'relative',
-        backgroundSize: 'cover',
-      }}>
-
-        <div style={{
-          position: 'absolute',
-          top: '30%',
-          left: '10%',
-          padding: 30,
-          borderRadius: 8,
-          fontSize: '2em',
-          backgroundColor: 'rgba(255, 150, 102, 0.875)',
-        }}>
-          Cutting Edge Web Code Curriculum
+      <div className='Home' style={{ backgroundImage: `url(${background})` }}>
+        <div className='banner'>
+          Cutting Edge <code>{'{Web Code}'}</code> Curriculum
+        </div>
+        <div className='frameworks'>
+          <img alt='ReactJS is the #1 client framework' src={reactjs}/>
+          <img alt='nodejs is the most popular server runtime' src={nodejs}/>
+          <div className='coming-soon'>
+            <img alt='Solidity makes Ethereum contracts programmable' src={solidity}/>
+          </div>
+        </div>
+        <div className='actions'>
+          <NavLink to='/works'>
+            <div>Read More about the courses</div>
+          </NavLink>
+          
+          <NavLink to='/personnel'>
+            <div>Who we are</div>
+          </NavLink>
+          
+          <NavLink to='/contact'>
+            <div>Get in touch, ask a question!</div>
+          </NavLink>
         </div>
       </div>
     );
