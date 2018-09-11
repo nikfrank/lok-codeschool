b296033c6e2ab4b5a3781497a5812e8b75bdf34e
nik frank <nikbfrank@gmail.com>
2018-09-12 00:25:25 +0300

    transition, icon, style home page

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,8 @@ import solidity from './Solidity.png';
 import nodejs from './nodejs.png';
 import reactjs from './reactjs.png';
 
+import ArrowRight from './ArrowRight.svg';
+
 import { NavLink } from 'react-router-dom';
 
 class Home extends Component {
@@ -16,7 +18,9 @@ class Home extends Component {
           Cutting Edge <code>{'{Web Code}'}</code> Curriculum
         </div>
         <div className='frameworks'>
-          <img alt='ReactJS is the #1 client framework' src={reactjs}/>
+          <div className='react-logo'>
+            <img alt='ReactJS is the #1 client framework' src={reactjs}/>
+          </div>
           <img alt='nodejs is the most popular server runtime' src={nodejs}/>
           <div className='coming-soon'>
             <img alt='Solidity makes Ethereum contracts programmable' src={solidity}/>
@@ -24,15 +28,15 @@ class Home extends Component {
         </div>
         <div className='actions'>
           <NavLink to='/works'>
-            <div>Read More about the courses</div>
+            <div>About the course <ArrowRight/></div>
           </NavLink>
           
           <NavLink to='/personnel'>
-            <div>Who we are</div>
+            <div>Who we are <ArrowRight/></div>
           </NavLink>
           
           <NavLink to='/contact'>
-            <div>Get in touch, ask a question!</div>
+            <div>Get in touch! <ArrowRight/></div>
           </NavLink>
         </div>
       </div>
