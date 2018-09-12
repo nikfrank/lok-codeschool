4597c1abd7b23cb2be2deee4ce4ff3cdec7d987a
nik frank <nikbfrank@gmail.com>
2018-09-12 19:40:43 +0300

    course topic listings

--- src/Works.js
+++ src/Works.js
@@ -11,55 +11,87 @@ class Works extends Component {
     return (
       <div className='Works' style={{ backgroundImage: `url(${background})` }}>
         <div className='shade'>
-          <div className='react js-course'>
+          <div className='react course'>
             <div className='react-logo'>
               <img src={reactLogo}/>
             </div>
-            <div className='xcoin-lesson'>
-              Bitcoin Charts
+            <div className='lesson'>
+              <h4>Bitcoin Charts</h4>
               <img/>
               <ul>
-                <li>JSX & CSS</li>
+                <li>React basics (JSX)</li>
+                <li>CSS basics in React</li>
                 <li>API calls -> live data</li>
                 <li>Chart plugins</li>
               </ul>
             </div>
-            <div className='snoop-lesson'>
-              Snoop's Job Application Form
+            <div className='lesson'>
+              <h4>Snoop's Job Application Form</h4>
               <img/>
               <ul>
-                <li>JSX & CSS</li>
-                <li>API calls -> live data</li>
-                <li>Chart plugins</li>
+                <li>Form Elements in JSX</li>
+                <li>Conditional Rendering</li>
+                <li>React Component & Props Basics</li>
+                <li>UX and User Driven Development</li>
               </ul>
             </div>
-            <div className='hmpg-lesson'>
-              My Portfolio Page
+            <div className='lesson'>
+              <h4>My Portfolio Page</h4>
               <img/>
               <ul>
-                <li>JSX & CSS</li>
-                <li>API calls -> live data</li>
-                <li>Chart plugins</li>
+                <li>React Client Side Routing</li>
+                <li>Responsive Design Principles</li>
+                <li>Common Use Cases in Web Design</li>
+                <li>Deployment to Personal Domain</li>
               </ul>
             </div>
-            <div>
-              github link
-              <br/>
-              <button>Sign up for waiting list</button>
+            <div className='links-actions'>
+              <div>github link</div>
+              <div>
+                <button className='signup-action'>Sign up for waiting list</button>
+              </div>
             </div>
           </div>
           
-          <div className='node js-course'>
+          <div className='node course'>
             <img src={nodeLogo}/>
+            <div>
+              <h4>NodeJS Course <span style={{color:'green'}}>In Alpha</span></h4>
+              <ul>
+                <li>ExpressJS RESTful API Service</li>
+                <li>Testing with POSTMAN, <code>supertest</code></li>
+                <li>Connecting to Databases & Services on the Cloud</li>
+                <li>Server-free Cloud Architecture with Lambda</li>
+              </ul>
+            </div>
           </div>
 
-          <div className='fullstack js-course'>
+          <div className='fullstack course'>
             <img src={reactLogo}/>
             <img src={nodeLogo}/>
+            <div>
+              <h4>FullStack JS Course <span style={{color:'green'}}>In Alpha</span></h4>
+              <ul>
+                <li>Full Start-up Workflow</li>
+                <li>Server and Front-end teams</li>
+                <li>Integration and Deployment</li>
+                <li>Creative Feature Designs</li>
+              </ul>
+            </div>
           </div>
           
-          <div className='solidity js-course'>
+          <div className='solidity course'>
             <img src={solidityLogo}/>
+            <div>
+              <h4>Solidity Course <span style={{color:'red'}}>Coming Soon</span></h4>
+              <ul>
+                <li>Local Testnet Workflows</li>
+                <li>Contract and Attack Basics</li>
+                <li>Integration with Browser Apps</li>
+                <li>Deployment to Ethereum Network</li>
+              </ul>
+            </div>
+
           </div>
         </div>
       </div>
